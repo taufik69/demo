d384a17f9ab1a3248252bbb76aa906782f22653d
taufik69 <taufik35-2915@diu.edu.bd>
2024-10-22 01:20:27 +0600

    cors polixy problem

--- app.js
+++ app.js
@@ -31,12 +31,13 @@ const app = express();
  */
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: "http://localhost:5173/",
-    credentials: true, // Allow cookies and credentials
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173/",
+//     credentials: true, // Allow cookies and credentials
+//   })
+// );
+app.use(cors());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
