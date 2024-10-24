7de4c6465df0f2f3a8216873795ca22c69174807
taufik69 <taufik35-2915@diu.edu.bd>
2024-10-24 22:22:06 +0600

    get news in reversed order

--- Controller/topNews.Controller.js
+++ Controller/topNews.Controller.js
@@ -5,46 +5,63 @@ const { ApiResponse } = require("../Utils/ApiResponse.js");
 const PosttopNews = async (req, res) => {
   try {
     const { id } = req.params;
-    const isExistTopNews = await topNewsModel.findOne({
-      // topNews: {
-      //   $elemMatch: { $eq: id },
-      // },
-      topNews: id,
-    });
-    if (isExistTopNews) {
-      return res
-        .status(401)
-        .json(new ApiError(401, null, `TopNews Is Already Exist !!`));
-    }
-    // Create a new instance of the topnews model
-    const newTopNews = new topNewsModel({
-      topNews: [id],
-    });
 
-    await newTopNews.save();
-    if (newTopNews) {
+    // Check if the topNews document exists
+    let topNewsDoc = await topNewsModel.findOne();
+
+    if (topNewsDoc) {
+      // Check if the ID is already in the array
+      const isIdExist = topNewsDoc.topNews.includes(id);
+
+      if (isIdExist) {
+        return res
+          .status(401)
+          .json(new ApiError(401, null, `TopNews Is Already Exist !!`));
+      }
+
+      // Push the new id into the topNews array and save
+      topNewsDoc.topNews.push(id);
+      await topNewsDoc.save();
+
       return res
         .status(200)
-        .json(new ApiResponse(200, newTopNews, "TopNews Create successfully!"));
+        .json(
+          new ApiResponse(200, topNewsDoc, "TopNews updated successfully!")
+        );
+    } else {
+      // If no topNews document exists, create a new one with the provided ID
+      const newTopNews = new topNewsModel({
+        topNews: [id],
+      });
+
+      await newTopNews.save();
+
+      return res
+        .status(200)
+        .json(
+          new ApiResponse(200, newTopNews, "TopNews created successfully!")
+        );
     }
-    return res
-      .status(401)
-      .json(new ApiError(401, null, "TopNews Create Failed"));
   } catch (error) {
-    return res.status(401).json(new ApiError(401, null, error.message));
+    return res.status(500).json(new ApiError(500, null, error.message));
   }
 };
 
 const GetAllTopNews = async (req, res) => {
   try {
-    // Find the topNews document by id and populate the topNews array
+    // Find all topNews documents and populate the topNews array, sort by 'createdAt' in descending order
     const topNews = await topNewsModel.find({}).populate("topNews");
 
-    if (!topNews) {
+    if (!topNews || topNews?.length === 0) {
       return res
         .status(404)
         .json(new ApiError(404, null, "TopNews not found!"));
     }
+
+    // Reverse the 'topNews' array inside each document
+    topNews.forEach((doc) => {
+      doc.topNews.reverse();
+    });
 
     return res
       .status(200)
