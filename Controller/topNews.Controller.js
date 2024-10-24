ae156db834ef6f04ab1ff26022128c79500c65d9
taufik69 <taufik35-2915@diu.edu.bd>
2024-10-24 22:33:59 +0600

    all done top news

--- Controller/topNews.Controller.js
+++ Controller/topNews.Controller.js
@@ -71,4 +71,78 @@ const GetAllTopNews = async (req, res) => {
   }
 };
 
-module.exports = { PosttopNews, GetAllTopNews };
+// update topnews controller
+const UpdateTopNews = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { newId } = req.body;
+
+    // Check if the topNews document exists
+    let topNewsDoc = await topNewsModel.findById(id);
+
+    if (!topNewsDoc) {
+      return res
+        .status(404)
+        .json(new ApiError(404, null, "TopNews document not found!"));
+    }
+
+    // Check if the new ID is already in the topNews array
+    const isIdExist = topNewsDoc.topNews.includes(newId);
+
+    if (isIdExist) {
+      return res
+        .status(400)
+        .json(new ApiError(400, null, "This news is already in the topNews !"));
+    }
+
+    // If not, add the new ID to the array and save the document
+    topNewsDoc.topNews.push(newId);
+    await topNewsDoc.save();
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, topNewsDoc, "TopNews updated successfully!"));
+  } catch (error) {
+    return res.status(500).json(new ApiError(500, null, error.message));
+  }
+};
+
+// delte top news
+const DeleteTopNews = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    // Find the topNews document
+    let topNewsDoc = await topNewsModel.findOne();
+
+    if (!topNewsDoc) {
+      return res
+        .status(404)
+        .json(new ApiError(404, null, "TopNews document not found!"));
+    }
+
+    // Find the index of the ID in the topNews array
+    const index = topNewsDoc.topNews.indexOf(id);
+
+    // If the ID doesn't exist in the array
+    if (index === -1) {
+      return res
+        .status(404)
+        .json(new ApiError(404, null, "ID not found in the topNews !"));
+    }
+
+    // Remove the ID from the array using splice
+    topNewsDoc.topNews.splice(index, 1); // Remove 1 element at the found index
+
+    // Save the updated document
+    await topNewsDoc.save();
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, topNewsDoc, "TopNews Deleted successfully!"));
+  } catch (error) {
+    return res.status(500).json(new ApiError(500, null, error.message));
+  }
+};
+
+module.exports = { PosttopNews, GetAllTopNews, UpdateTopNews, DeleteTopNews };
--- Routes/Api/topNews.ApiRoutes.js
+++ Routes/Api/topNews.ApiRoutes.js
@@ -2,10 +2,14 @@ const express = require("express");
 const {
   PosttopNews,
   GetAllTopNews,
+  UpdateTopNews,
+  DeleteTopNews,
 } = require("../../Controller/topNews.Controller.js");
 const _ = express.Router();
 
 _.route("/topNews/:id").post(PosttopNews);
 _.route("/get-all-topNews").get(GetAllTopNews);
+_.route("/update-topNews/:id").patch(UpdateTopNews);
+_.route("/delete-topNews/:id").delete(DeleteTopNews);
 
 module.exports = _;
