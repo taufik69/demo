0ba76a097344ff1252641d705c95f813b8de7635
taufik69 <taufik35-2915@diu.edu.bd>
2024-11-12 17:18:08 +0600

    changing db configration

--- Database/index.js
+++ Database/index.js
@@ -1,27 +1,28 @@
 const mongoose = require("mongoose");
 const { dbName } = require("../constants");
 
-// const DbConnection = async () => {
-//   try {
-//     const databaseInstance = await mongoose.connect(
-//       `${process.env.DATABASE_URL}/${dbName}`
-//     );
-//     console.log(
-//       `Database Connection Successfull ${databaseInstance.connection.host}`
-//     );
-//   } catch (error) {
-//     console.log(`Database Connection Error ${error}`);
-//   }
-// };
-
 const DbConnection = async () => {
-  mongoose
-    .connect(`${process.env.DATABASE_URL}/${dbName}`)
-    .then((databaseInstance) => {
-      console.log(`Database Connection Successfull `);
-    })
-    .catch((error) => {
-      console.log(`Database Connection Error ${error}`);
+  try {
+    await mongoose.connect(`${process.env.DATABASE_URL}/${dbName}`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      connectTimeoutMS: 10000, // 10 seconds connection timeout
+      serverSelectionTimeoutMS: 10000, // 10 seconds timeout for selecting MongoDB server
     });
+    console.log("Database Connection Successful");
+  } catch (error) {
+    console.error("Database Connection Error:", error);
+  }
 };
+
+// const DbConnection = async () => {
+//   mongoose
+//     .connect(`${process.env.DATABASE_URL}/${dbName}`)
+//     .then((databaseInstance) => {
+//       console.log(`Database Connection Successfull `);
+//     })
+//     .catch((error) => {
+//       console.log(`Database Connection Error ${error}`);
+//     });
+// };
 module.exports = { DbConnection };
